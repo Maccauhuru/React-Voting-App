6d90ed7ba13dceae75d72ed561441cc1f2a70624
S.L.Mupfukudzwa <simba3310@gmail.com>
2018-05-22 18:24:52 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+class HelloWorld extends React.Component{
+render(){
+    return(<h1>Hello React World,Im ready for you!!!</h1>);
+}
+}
+
+ReactDOM.render(<HelloWorld/>,document.getElementById("content"));
+
+
