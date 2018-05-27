2ecebe2e30c9553742c4840c1a2cc3a891654c04
S.L.Mupfukudzwa <simba3310@gmail.com>
2018-05-27 17:27:10 -0500

    modified URL's for images to newly added ones

--- public/js/seeds.js
+++ public/js/seeds.js
@@ -1,17 +1,17 @@
-window.Seed = (function () {
+window.Seeds = (function () {
   function generateVoteCount() {
     return Math.floor((Math.random() * 50) + 15);
   }
 
-  const products = [
+  const episodes = [
     {
       id: 1,
       title: 'Grow Fast or Die Slow',
       description: "The pressure to get things right stymies Richard and forces him to grow the company in a way he had not planned. Dinesh and Gilfoyle question their ability to make good decisions.Gavin returns to Hooli and worries about becoming antiquated.",
       url: '#',
       votes: generateVoteCount(),
-      submitterAvatarUrl: 'images/avatars/daniel.jpg',
-      productImageUrl: 'images/products/image-aqua.png',
+      submitterAvatarUrl: 'images/avatars/becky.jpg',
+      episodeImageUrl: 'images/products/episode1.jpg',
     },
     {
       id: 2,
@@ -19,20 +19,20 @@ window.Seed = (function () {
       description: 'Richard manages a number of small conflicts in his efforts to unite his new employees.Dinesh celebrates a new purchase that Gilfoyle looks to spoil.Jian - Yang goes to court.Gavin meets pushback over his signature and what it says about him.',
       url: '#',
       votes: generateVoteCount(),
-      submitterAvatarUrl: 'images/avatars/kristy.png',
-      productImageUrl: 'images/products/image-rose.png',
+      submitterAvatarUrl: 'images/avatars/cecil.jpg',
+      episodeImageUrl: 'images/products/episode2.jpg',
     },
     {
       id: 3,
       title: 'Chief Operating Officer',
-        description: 'Encouraged by Jared to strike up a friendship with Dana, a like-minded CEO, Richard instead finds himself charmed by Danas COO, who challenges his loyalty to Gilfoyle; Dineshs impulse purchase forces him to search for a new roommate.',
+      description: 'Encouraged by Jared to strike up a friendship with Dana, a like-minded CEO, Richard instead finds himself charmed by Danas COO, who challenges his loyalty to Gilfoyle; Dineshs impulse purchase forces him to search for a new roommate.',
       url: '#',
       votes: generateVoteCount(),
-      submitterAvatarUrl: 'images/avatars/veronika.jpg',
-      productImageUrl: 'images/products/image-steel.png',
+      submitterAvatarUrl: 'images/avatars/nick.png',
+      episodeImageUrl: 'images/products/episode3.jpg',
     }
-    
+
   ];
 
-  return { products: products };
+  return { episodes: episodes };
 }());
