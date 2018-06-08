29daea01e79293f58000eff3878e31bb5cd02454
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-08 18:06:10 -0500

    change state of EpisodesList

--- public/js/app.js
+++ public/js/app.js
@@ -1,109 +1,112 @@
 //initialize our state to an object with an empty products array.
 class EpisodesList extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            episodes : [],
-        };
-    }
-        componentDidMount() {
-            this.setState({episodes : Seeds.episodes});
-        };
-    
-    handleUpvoteBtnClick(episodeId) {
-        console.log(`${episodeId} was clicked.`)  
+  constructor(props) {
+    super(props);
+    this.state = {
+      episodes: []
     };
-    handleDownVoteBtnClick(episodeTitle){
-        console.log(`You downvoted the "${episodeTitle}" Episode!`);
-    }
+    this.handleUpvoteBtnClick = this.handleUpvoteBtnClick.bind(this);
+    //this.handleEpisodeDownVote = this.handleEpisodeDownVote.bind(this);
+  }
+  componentDidMount() {
+    this.setState({ episodes: Seeds.episodes });
+  }
 
-    render() { 
-       const episodes = this.state.episodes.sort((a,b)=>(
-           b.votes - a.votes
-        ));
-        const episodeComponents = Seeds.episodes.map((episode) =>
-            (
-                <Episode
-                    key={'episode' + episode.id}
-                    id={episode.id}
-                    title={episode.title}
-                    description={episode.description}
-                    url={episode.url}
-                    votes={episode.votes}
-                    submitterAvatarUrl={episode.submitterAvatarUrl}
-                    episodeImageUrl={episode.episodeImageUrl}
-                    onUpVote= {this.handleUpvoteBtnClick}
-                    onDownVote={this.handleDownVoteBtnClick}
-                />
-            ));
-        return (
-            <div className="ui unstackable items">
-                {episodeComponents}
-            </div>
-        );
-    }
+  handleUpVoteBtnClick(episodeId) {
+ const nextEpisodes = this.state.episodes.map((episode) => {
+   if (episode.id === episodeId) {
+     return Object.assign({}, episode, {
+       votes: episode.votes + 1,
+     });
+   } else {
+     return episode;
+   }
+ });
+
+ this.setState({
+   episodes: nextEpisodes,
+ });
+
+  }
+  handleDownVoteBtnClick(episodeTitle) {
+    console.log(`You downvoted the "${episodeTitle}" Episode!`);
+  }
+//inside EpisodesList
+
+
+
+  render() {
+    const episodes = this.state.episodes.sort((a, b) => b.votes - a.votes);
+    const episodeComponents = Seeds.episodes.map(episode => (
+      <Episode
+        key={"episode" + episode.id}
+        id={episode.id}
+        title={episode.title}
+        description={episode.description}
+        url={episode.url}
+        votes={episode.votes}
+        submitterAvatarUrl={episode.submitterAvatarUrl}
+        episodeImageUrl={episode.episodeImageUrl}
+        onUpVote={this.handleUpVoteBtnClick}
+        onDownVote={this.handleDownVoteBtnClick}
+      />
+    ));
+    return <div className="ui unstackable items">{episodeComponents}</div>;
+  }
 }
 
 class Episode extends React.Component {
-    constructor(props){
-        super(props);
-        this.handleUpVote = this.handleUpVote.bind(this);
-        this.handleDownVote = this.handleDownVote.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.handleUpVote = this.handleUpVote.bind(this);
+    this.handleDownVote = this.handleDownVote.bind(this);
+  }
 
-    //Inside 'Episode'
-    handleUpVote(){
-        this.props.onUpVote(this.props.id)
-    }
+  //Inside 'Episode'
+  handleUpVote() {
+    this.props.onUpVote(this.props.id);
+  }
 
-    handleDownVote (){
-        this.props.onDownVote(this.props.title)
-    }
-    render() {
-        let redBtnStyle ={
-            color:'red'
-        }
-        let greenBtnStyle = {
-            color: 'green'
-        }
-        return (
-            <div className='item'>
-                <div className='image'>
-                    <img src={this.props.episodeImageUrl} />
-                </div>
-                {/*apply style to buttons on components*/}
-                <div className='middle aligned content'>
-                    <div className='header'>
-                        <a onClick={this.handleUpVote}>
-                            <i className='large caret up icon' style={greenBtnStyle} />
-                        </a>
-                        {this.props.votes}
-                        <a onClick={this.handleDownVote}>
-                            <i className='large caret down icon' style={redBtnStyle}/>
-                        </a>
-                       
-                    </div>
-                    <div className='description'>
-                        <a href={this.props.url}>
-                            {this.props.title}
-                        </a>
-                        <p>
-                            {this.props.description}
-                        </p>
-                    </div>
-                    <div className='extra'>
-                        <span>Submitted by:</span>
-                        <img
-                            className='ui avatar image'
-                            src={this.props.submitterAvatarUrl}
-                        />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  handleDownVote() {
+    this.props.onDownVote(this.props.title);
+  }
+  render() {
+    let redBtnStyle = {
+      color: "red"
+    };
+    let greenBtnStyle = {
+      color: "green"
+    };
+    return (
+      <div className="item">
+        <div className="image">
+          <img src={this.props.episodeImageUrl} />
+        </div>
+        {/*apply style to buttons on components*/}
+        <div className="middle aligned content">
+          <div className="header">
+            <a onClick={this.handleUpVote}>
+              <i className="large caret up icon" style={greenBtnStyle} />
+            </a>
+            {this.props.votes}
+            <a onClick={this.handleDownVote}>
+              <i className="large caret down icon" style={redBtnStyle} />
+            </a>
+          </div>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img
+              className="ui avatar image"
+              src={this.props.submitterAvatarUrl}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 ReactDOM.render(<EpisodesList />, document.getElementById("content"));
-
-
